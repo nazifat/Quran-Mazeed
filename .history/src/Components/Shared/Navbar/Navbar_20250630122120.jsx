152b57d3f50f50a5c466434a54490d9eda360e15
nazifat <nazifa.iit@gmail.com>
2025-06-30 12:26:50 +0600

    navbar fixed

--- .history/src/Components/Shared/Navbar/Navbar_20250630122120.jsx
+++ .history/src/Components/Shared/Navbar/Navbar_20250630122120.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
     </>
     return (
-        <div className="navbar  fixed top-0 z-50 bg-[#ffffff]  shadow-sm custom-navbar md:px-10 font-inter">
+        <div className="navbar fixed top-0 shadow-sm custom-navbar md:px-10 font-inter">
             <div className="navbar-start ">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
